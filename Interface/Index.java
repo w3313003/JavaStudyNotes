a7914cfcb7c8785f2b45478a232f67ffe15a8681
zj <w3313003@163.com>
2018-05-16 01:29:21 +0800

    追加lambda

--- Interface/Index.java
+++ Interface/Index.java
@@ -5,7 +5,15 @@ package Interface;
  * 在接口中定义属性的知识点：
  * 1，属性默认的修饰词是：public static final
  * 2，定义的时候必须初始化。
- * 3，在接口中定义的属性 等同于 常量，接口中不允许定义变量。
+ * 3，在接口中定义的属性 等同于 常量，接口中不允许定义变量
+ * 4, default修饰符表示默认方法，实现类中可以重新定义同名方法以覆盖默认方法
+ * @解决默认方法冲突
+ * 1 ) 超类优先。如果超类提供了一个具体方法，同名而且有相同参数类型的默认方法会被忽略。
+ * 2 ) 接口冲突。 如果一个超接口提供了个默认方法，另个接口提供了个同名而且参数类型（不论是否是默认参数）相同的方法，
+ * 实现类必须覆盖这个方法来解决冲突。
+ * eg: return Super.super.getName();
+ * 3 )一个类扩展了一个超类，同时实现了一个接口，并从超类和接口继承了相同的方法。则只会考虑超类方法，接口的所有默认方法都会被忽略
+ * 类优先原则
  */
 
 import java.util.Arrays;
@@ -20,16 +28,16 @@ public class Index {
         }
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
-        Short s1 = 129;
-        Short s2 = 129;
-        System.out.println(s1 == s2);
+        System.out.println(new Car().test());
     }
+    
 }
 
 interface Test {
     String name = "213";
-    void move();
-
+    default int test() {
+        return 123;
+    }
 }
 
 class Super {
@@ -37,24 +45,42 @@ class Super {
     public Super() {
         Super.count++;
     }
+    public int test() {
+        return 789;
+    }
 }
-class Car extends Super implements Comparable<Car> {
-    private String name;
+class Car extends Super implements Comparable<Car>, Test {
+    public String name;
     public int age;
     public Car() {
-        this.name = "123";
         this.age = super.count;
     }
     public Car(String name) {
         this.name = name;
         this.age = super.count;
     }
-//      实现Comparable接口的compareTo方法 如果调用Arrays.sort 则类必须实现compareTo方法
+    //      实现Comparable接口的compareTo方法 如果调用Arrays.sort 则类必须实现compareTo方法
     @Override
     public int compareTo(Car o) {
 //        类型强转 if params = (Object o) -> Car c = (Car) o;
         return Integer.compare(this.age, o.age);
     }
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getAge() {
+        return this.age;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
     public boolean equals(Object o) {
         if (o == this) return true;
         if (!(o instanceof Car)) return false;
@@ -66,7 +92,7 @@ class Car extends Super implements Comparable<Car> {
         if (this.age != other.age) return false;
         return true;
     }
-
+    
     public int hashCode() {
         final int PRIME = 59;
         int result = 1;
@@ -75,11 +101,11 @@ class Car extends Super implements Comparable<Car> {
         result = result * PRIME + this.age;
         return result;
     }
-
+    
     protected boolean canEqual(Object other) {
         return other instanceof Car;
     }
-
+    
     public String toString() {
         return "Car(name=" + this.name + ", age=" + this.age + ")";
     }
--- lambda.java
+++ lambda.java
@@ -1,23 +1,35 @@
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.Timer;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
+/**
+ * 只有一个抽象方法的接口被称为函数式接口。可以用lambda表达式来替代实现类实例
+ *
+ */
 public class lambda {
-    public static void main(String str[]) {
+    public static void main(String args[]) {
         Integer[] a = new Integer[20];
         for(int i = 0; i <= a.length - 1; i++) {
             a[i] = i;
-        };
-        List<Integer> list =  Arrays.asList(a);
-        list.forEach(i -> {
-            i += 2;
-            System.out.println(i);
-        });
+        }
+        ArrayList<Integer> list = new ArrayList(Arrays.asList(a));
+//        List<Integer> list =  Arrays.asList(a);
+        list.forEach(i -> list.add(i * 3));
+        list.removeIf(e -> e % 2 == 0);
         System.out.println(Arrays.toString(list.toArray()));
         Timer timer = new Timer();
         String[] arr = {"1222", "21", "1"};
-//        比较器为lambad表达式 实现数组自定义排序 另一种方式将比较器构造为实现了Comparator的类实例。
+//        比较器为lambda表达式 实现数组自定义排序 另一种方式将比较器构造为实现了Comparator的类实例。
         Arrays.sort(arr, (String a1, String b) -> a1.length() - b.length());
         System.out.println(Arrays.toString(arr));
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+            
+            }
+        }, 100);
     }
 }
